f373bf007508e29cd13c2907af58bd54f62fe974
PHUNG NGHIA <phungnghia531@gmail.com>
2026-03-10 13:05:21 +0700

    Init

--- src/index.jsx
+++ src/index.jsx
@@ -4,14 +4,14 @@ import "./index.css";
 import App from "./App.jsx";
 import "virtual:uno.css";
 const root = document.getElementById("root");
-import Introduction from "./pages/Introduction.jsx";
+import Introduction from "../pages/Introduction.jsx";
 import { Router, Route } from "@solidjs/router";
-import Projects from "./pages/Project.jsx";
-import Contacts from "./pages/Contacts.jsx";
+import Projects from "../pages/Project.jsx";
+import Contacts from "../pages/Contacts.jsx";
 
 render(
   () => (
-    <Router root={App} base="/Portfolio_New/">
+    <Router root={App}>
       <Route path="/" component={Introduction} />
       <Route path="/projects" component={Projects} />
       <Route path="/contacts" component={Contacts} />
